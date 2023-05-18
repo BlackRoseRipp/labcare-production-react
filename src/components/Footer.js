e95c5f90d1386257ef6a24716d5c58d8e0157755
unknown <unk1222@gmail.com>
2023-05-18 14:13:21 -0400

    Finished Home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,195 @@
+import { Fragment } from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa";
 import { useLocation } from "react-router-dom";
 import { pages } from "./routes";
 
 const Footer = () => {
   const location = useLocation();
   const pathname = location.pathname;
+  const classNames = (...classes) => {
+    return classes.filter(Boolean).join(" ");
+  };
 
   return pages.some((page) => page.path === pathname) ? (
-    <footer className="py-8 mt-auto bg-green">
-      <div className="mx-8">
-        <div className="grid lg:grid-cols-3 gap-8">
-          <div className="flex md:flex-row flex-col items-center lg:justify-start justify-center gap-4 lg:order-1 order-2">
-            <a href="/plans" className="text-white hover:text-amber">
-              Our Preset Plans
-            </a>
-            <a href="#" className="text-white hover:text-amber">
-              Book a Reservation
-            </a>
-            <a href="/contact" className="text-white hover:text-amber">
-              Contact
-            </a>
-          </div>
-          <div className="flex items-center justify-center w-fit mx-auto justify-self-center lg:order-2 order-1">
-            <h2 className="logo text-white xl:text-6xl md:text-5xl text-3xl font-bold">
-              Shalvah
-            </h2>
-          </div>
-          <div className="flex flex-col lg:items-end items-center justify-center order-3">
-            <a href="#" className="text-white hover:text-amber">
-              (123) 456-7890
-            </a>
-            <a href="#" className="text-white hover:text-amber">
-              help@happymothers.com
-            </a>
-            <a href="#" className="text-white hover:text-amber">
-              Our Location
-            </a>
+    <Fragment>
+      <footer className="py-12 mt-auto bg-blue-primary">
+        <div className="max-w-screen-xl lg:mx-auto mx-8">
+          <div className="grid lg:grid-cols-3 gap-4">
+            <div className="flex flex-col justify-center gap-4">
+              <a href="/" className="w-1/3">
+                <img src="/img/LabQ-Logo-White.svg" className="w-full" />
+              </a>
+              <div className="flex items-center gap-x-2">
+                <a
+                  className="px-2 text-white hover:text-blue-secondary"
+                  href="https://www.instagram.com/labqdx/"
+                >
+                  <FaInstagram />
+                </a>
+                <a
+                  className="px-2 text-white hover:text-blue-secondary"
+                  href="https://www.linkedin.com/company/skybro/"
+                >
+                  <FaLinkedinIn />
+                </a>
+                <a
+                  className="px-2 text-white hover:text-blue-secondary"
+                  href="https://www.facebook.com/labqdiagnostics"
+                >
+                  <FaFacebookF />
+                </a>
+                <a
+                  className="px-2 text-white hover:text-blue-secondary"
+                  href="https://twitter.com/LabQ247"
+                >
+                  <FaTwitter />
+                </a>
+              </div>
+              <div className="flex items-center gap-x-8 mt-10">
+                <a href="https://apps.apple.com/us/app/labq/id1610376669">
+                  <img
+                    className="w-[120px] h-10"
+                    src="/img/icons/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
+                  />
+                </a>
+                <a href="https://play.google.com/store/apps/details?id=com.labq&pli=1">
+                  <img
+                    className="w-[120px] h-10"
+                    src="/img/icons/Asset-1.svg"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <h3 className="heading text-blue-secondary text-3xl font-medium">
+                Navigation
+              </h3>
+              <div className="flex gap-x-12 mt-10">
+                <div className="flex flex-col gap-y-2">
+                  <a
+                    href="/"
+                    className={classNames(
+                      pathname === "/"
+                        ? "text-blue-secondary"
+                        : "text-white hover:text-blue-secondary"
+                    )}
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="/location"
+                    className={classNames(
+                      pathname === "/location"
+                        ? "text-blue-secondary"
+                        : "text-white hover:text-blue-secondary"
+                    )}
+                  >
+                    Locations
+                  </a>
+                  <a
+                    href="/about"
+                    className={classNames(
+                      pathname === "/about"
+                        ? "text-blue-secondary"
+                        : "text-white hover:text-blue-secondary"
+                    )}
+                  >
+                    About
+                  </a>
+                  <a
+                    href="/news"
+                    className={classNames(
+                      pathname === "/news"
+                        ? "text-blue-secondary"
+                        : "text-white hover:text-blue-secondary"
+                    )}
+                  >
+                    News
+                  </a>
+                </div>
+                <div className="flex flex-col gap-y-2">
+                  <a
+                    href="/tests"
+                    className={classNames(
+                      pathname === "/tests"
+                        ? "text-blue-secondary"
+                        : "text-white hover:text-blue-secondary"
+                    )}
+                  >
+                    Tests
+                  </a>
+                  <a
+                    href="/careers"
+                    className={classNames(
+                      pathname === "/careers"
+                        ? "text-blue-secondary"
+                        : "text-white hover:text-blue-secondary"
+                    )}
+                  >
+                    Careers
+                  </a>
+                  <a
+                    href="/contact-us"
+                    className={classNames(
+                      pathname === "/contact-us"
+                        ? "text-blue-secondary"
+                        : "text-white hover:text-blue-secondary"
+                    )}
+                  >
+                    Contact
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center">
+              <h3 className="heading text-blue-secondary text-3xl font-medium mb-12">
+                Subscribe to our Newsletter
+              </h3>
+              <input
+                type="email"
+                className="px-4 py-2 w-4/5"
+                id="newsletterEmailInput"
+                placeholder="Email"
+              />
+              <button className="bg-blue-secondary text-white mt-6 w-4/5 text-center font-bold px-4 py-2">
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+      <footer className="bg-white py-4">
+        <div className="max-w-screen-xl lg:mx-auto mx-8 flex items-center justify-center">
+          <p className="text-blue-primary font-medium mx-4">
+            &copy; LabCare Diagnostics. All Right Reserved{" "}
+            {new Date().getFullYear()}.
+          </p>
+          <a
+            href="/privacy-policy"
+            className="border-l border-neutral-300 px-4 text-blue-primary font-medium"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="/terms-and-conditions"
+            className="border-l border-neutral-300 px-4 text-blue-primary font-medium"
+          >
+            Terms & Conditions
+          </a>
+          <a
+            href="/accessibility-statement"
+            className="border-l border-neutral-300 px-4 text-blue-primary font-medium"
+          >
+            Accessibility Statement
+          </a>
+        </div>
+      </footer>
+    </Fragment>
   ) : null;
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,10 @@
 import { Fragment } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-import { BsCheck2, BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
-import {
-  FaHandHoldingHeart,
-  FaRegClock,
-  FaFacebook,
-  FaMapMarkedAlt,
-} from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-import { IoIosBed } from "react-icons/io";
-import { IoRibbonOutline } from "react-icons/io5";
 import { BiRun } from "react-icons/bi";
+import { BsFileBarGraph, BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
+import { FaFacebook, FaMapMarkedAlt } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { IoRibbonOutline } from "react-icons/io5";
 import LAB_SPECIALTIES from "../store/LAB_SPECIALTIES.json";
 
 const Home = () => {
@@ -175,8 +169,8 @@ const Home = () => {
         </div>
       </section>
       <section className="grid grid-cols-2">
-        <div className="bg-[#F4F9FF] flex flex-col items-end justify-center gap-y-8 p-2">
-          <div className="flex group max-w-md">
+        <div className="bg-[#F4F9FF] flex flex-col items-end justify-center gap-y-8 pr-2 pl-56 py-20">
+          <div className="flex group">
             <BiRun className="text-amber-400 bg-orange-200 rounded-full shrink-0 w-[50px] h-[50px] p-2" />
             <div className="flex flex-col w-fit ml-4">
               <h3 className="group-hover:text-blue-secondary text-neutral-800 text-2xl font-medium transition-all duration-300">
@@ -185,15 +179,15 @@ const Home = () => {
               <p>
                 Waiting for test results has got to be the most nerve wracking
                 experience of all time. Which is why speed takes a center stage
-                at LabQ. We’re proud to have one of the fastest turnaround track
-                record in the entire Tri State area, so that you can hold the
-                test results in your hands within a few hours from the time it
-                was administered. What does that mean for you?{" "}
+                at LabCare. We’re proud to have one of the fastest turnaround
+                track record in the entire Tri State area, so that you can hold
+                the test results in your hands within a few hours from the time
+                it was administered. What does that mean for you?{" "}
                 <span className="font-bold">Fast results.</span>
               </p>
             </div>
           </div>
-          <div className="flex group max-w-md">
+          <div className="flex group">
             <IoRibbonOutline className="text-red-500 bg-red-300 rounded-full shrink-0 w-[50px] h-[50px] p-2" />
             <div className="flex flex-col w-fit ml-4">
               <h3 className="group-hover:text-blue-secondary text-neutral-800 text-2xl font-medium transition-all duration-300">
@@ -208,7 +202,7 @@ const Home = () => {
               </p>
             </div>
           </div>
-          <div className="flex group max-w-md">
+          <div className="flex group">
             <FaMapMarkedAlt className="text-green-500 bg-green-600/30 rounded-full shrink-0 w-[50px] h-[50px] p-2 overflow-visible" />
             <div className="flex flex-col w-fit ml-4">
               <h3 className="group-hover:text-blue-secondary text-neutral-800 text-2xl font-medium transition-all duration-300">
@@ -223,25 +217,136 @@ const Home = () => {
               </p>
             </div>
           </div>
-          <div className="flex group max-w-md">
-            <BiRun className="text-amber-400 bg-orange-200 rounded-full shrink-0 w-[50px] h-[50px] p-2" />
+          <div className="flex group">
+            <BsFileBarGraph className="text-sky-500 bg-sky-400/70 rounded-full shrink-0 w-[50px] h-[50px] p-2 overflow-visible" />
             <div className="flex flex-col w-fit ml-4">
               <h3 className="group-hover:text-blue-secondary text-neutral-800 text-2xl font-medium transition-all duration-300">
-                It's about time.
+                Clarity is Key.
               </h3>
               <p>
-                Waiting for test results has got to be the most nerve wracking
-                experience of all time. Which is why speed takes a center stage
-                at LabQ. We’re proud to have one of the fastest turnaround track
-                record in the entire Tri State area, so that you can hold the
-                test results in your hands within a few hours from the time it
-                was administered. What does that mean for you?{" "}
-                <span className="font-bold">Fast results.</span>
+                Your test results should be easy to access and easier to
+                decipher. No who-can-read-this jargon or high falutin terms to
+                google. All you need to do is log in to your account and you’ll
+                find your easy-to- read test results staring right at you. What
+                does that mean for you?{" "}
+                <span className="font-bold">
+                  Clear results, easy to access.
+                </span>
               </p>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center"></div>
+        <div className="flex flex-col items-center justify-center pr-56">
+          <h2 className="text-4xl text-center font-medium heading">
+            Your Local Lab
+          </h2>
+        </div>
+      </section>
+      <section className="container mx-auto py-16">
+        <div
+          id="newsBlogControls"
+          className="relative w-4/5 mx-auto"
+          data-te-carousel-init
+          data-te-carousel-slide
+        >
+          {/* <!--Carousel items--> */}
+          <div className="relative w-[90%] mx-auto overflow-hidden after:clear-both after:block after:content-['']">
+            {/* <!--First item--> */}
+            <div
+              className="relative flex justify-center float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+              data-te-carousel-item
+              data-te-carousel-active
+            >
+              <div className="grid grid-cols-2 gap-8">
+                <div className="flex items-center justify-center">
+                  <a
+                    className="rounded-xl overflow-hidden group"
+                    href="/labq-ribbon-cutting-ceremony-for-newest-lab-and-data-hub-in-mt-olive"
+                  >
+                    <img
+                      className="w-full group-hover:scale-110 transition-all"
+                      src="/img/news/189195214_4165785780154267_6720674629117732980_n.jpg"
+                    />
+                  </a>
+                </div>
+                <div className="flex flex-col justify-center">
+                  <a
+                    href="/category/news"
+                    className="rounded-full py-1 px-4 text-xs font-medium text-blue-secondary border border-black w-fit"
+                  >
+                    News
+                  </a>
+                  <a
+                    href="/labq-ribbon-cutting-ceremony-for-newest-lab-and-data-hub-in-mt-olive"
+                    className="text-3xl font-semibold"
+                  >
+                    LabQ Ribbon Cutting Ceremony for newest lab and data hub in
+                    Mt. Olive.
+                  </a>
+                  <p className="text-neutral-500 my-4">
+                    PRESS RELEASE Mt. Olive, NJ – On Wednesday, May 19th, HQ
+                    Health partnered with Robert Greenbaum, the mayor of Mt.
+                    Olive, to host an Open House and Ribbon Cutting. The event
+                    was held for the new HQ Center at 100 International Dr., in
+                    the International Trade Center in Mt....
+                  </p>
+                  <a
+                    className="text-sm font-medium transition-all hover:text-blue-secondary"
+                    href="/labq-ribbon-cutting-ceremony-for-newest-lab-and-data-hub-in-mt-olive"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* <!--Second item--> */}
+            {/* <div
+              className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+              data-te-carousel-item
+            >
+              <img
+                src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+                className="block w-full"
+                alt="Camera"
+              />
+            </div>
+            <!--Third item-->
+            <div
+              className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+              data-te-carousel-item
+            >
+              <img
+                src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+                className="block w-full"
+                alt="Exotic Fruits"
+              />
+            </div> */}
+          </div>
+
+          {/* <!--Carousel controls - prev item--> */}
+          <button
+            className="absolute overflow-hidden bottom-0 left-0 top-0 z-[1] flex w-8 items-center justify-center border-0 bg-none p-0 text-center text-white transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:outline-none focus:text-white focus:no-underline focus:outline-none motion-reduce:transition-none group"
+            type="button"
+            data-te-target="#newsBlogControls"
+            data-te-slide="prev"
+          >
+            <span className="inline-block h-8 w-8 mr-16 before:bg-[#3892e7] before:rotate-[-60deg] before:content-[''] before:w-[16px] before:h-[3px] before:absolute before:left-1/4 after:bg-[#3892e7] after:rotate-[60deg] after:content-[''] after:w-[16px] after:h-[3px] after:absolute after:left-1/4 after:translate-y-3 before:group-hover:-rotate-45 after:group-hover:rotate-45 after:group-hover:translate-y-[11px] before:group-hover:translate-y-[1px] before:transition-all after:transition-all"></span>
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Previous
+            </span>
+          </button>
+          <button
+            className="absolute overflow-hidden bottom-0 right-0 top-0 z-[1] flex w-8 items-center justify-center border-0 bg-none p-0 text-center text-white transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:outline-none focus:text-white focus:no-underline focus:outline-none motion-reduce:transition-none group"
+            type="button"
+            data-te-target="#newsBlogControls"
+            data-te-slide="next"
+          >
+            <span className="inline-block h-8 w-8 mr-16 before:bg-[#3892e7] before:rotate-[60deg] before:content-[''] before:w-[16px] before:h-[3px] before:absolute before:left-1/4 after:bg-[#3892e7] after:rotate-[-60deg] after:content-[''] after:w-[16px] after:h-[3px] after:absolute after:left-1/4 after:translate-y-3 before:group-hover:rotate-45 after:group-hover:-rotate-45 after:group-hover:translate-y-[11px] before:group-hover:translate-y-[1px] before:transition-all after:transition-all"></span>
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Next
+            </span>
+          </button>
+        </div>
       </section>
     </Fragment>
   );
