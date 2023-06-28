9e335d604256e5e2bb8168ad7a869d6067ba13dd
unknown <unk1222@gmail.com>
2023-06-28 16:25:35 -0400

    Added animated text and almost finished banner slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,133 +1,274 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BiRun } from "react-icons/bi";
 import { BsFileBarGraph, BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
 import { FaFacebook, FaMapMarkedAlt } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { IoRibbonOutline } from "react-icons/io5";
+import { RxCaretRight } from "react-icons/rx";
 import BlogSlider from "../components/BlogSlider";
 import useWindowSize from "../hooks/useWindowSize";
 import LAB_SPECIALTIES from "../store/LAB_SPECIALTIES.json";
 
 const Home = () => {
+  const [isVisible, setIsVisible] = useState(0);
+
   const size = useWindowSize();
   const classNames = (...classes) => {
     return classes.filter(Boolean).join(" ");
   };
   const num = size.width < 1024 ? [1, 2, 5, 6, 9, 10] : [1, 3, 4, 6, 9, 11];
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsVisible((isVisible) => (isVisible < 3 ? isVisible + 1 : 0));
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const yourLabArr = [
+    "Qualified Staff",
+    "Quick Results",
+    "Quality Control",
+    "Quantity of Services",
+  ];
+
   return (
     <Fragment>
-      <section className="bg-[url(../public/img/landing22.jpg)] bg-cover bg-center bg-no-repeat">
-        <div className="flex flex-col items-center justify-center py-5 max-w-screen-lg mx-auto">
-          <h1 className="heading text-blue-primary font-semibold text-5xl text-center my-4">
-            COVID-19 Mobile Testing Services
-          </h1>
-          <div className="bg-amber-300 p-3">
-            <p className="text-lg font-medium text-center">
-              ** Please note: Beginning May 10th, a prescription from your
-              Doctor will be required for covid testing.
-            </p>
+      <div
+        id="homeBanner"
+        class="relative"
+        data-te-carousel-init
+        data-te-carousel-slide
+      >
+        <div
+          class="absolute bottom-0 left-0 right-0 z-[2] list-none justify-center gap-8 bg-black/20 max-w-screen-xl rounded-lg mx-auto mt-12 py-4 px-12 md:opacity-100 opacity-0 md:w-full w-0 overflow-hidden sm:flex hidden"
+          data-te-carousel-indicators
+        >
+          <button
+            type="button"
+            data-te-target="#homeBanner"
+            data-te-slide-to="0"
+            data-te-carousel-active
+            class="flex items-center justify-end gap-6 text-white font-semibold px-4 text-2xl border-r border-white"
+            aria-current="true"
+            aria-label="Slide 1"
+          >
+            <img
+              src="/img/icons/labq-tests1.svg"
+              className="h-[50px] w-auto mr-4"
+            />
+            <span className="hover:text-slate-100 transition-all">
+              {" "}
+              Mobile Labs{" "}
+            </span>
+          </button>
+          <button
+            type="button"
+            data-te-target="#homeBanner"
+            data-te-slide-to="1"
+            class="flex items-center justify-center gap-6 text-white font-semibold px-4 text-2xl"
+            aria-label="Slide 2"
+          >
+            <img
+              src="/img/icons/labq-icon.png"
+              className="h-[50px] w-auto mr-4"
+            />
+            <span className="hover:text-slate-100 transition-all">
+              Automation & Innovation
+            </span>
+          </button>
+          <button
+            type="button"
+            data-te-target="#homeBanner"
+            data-te-slide-to="2"
+            class="flex items-center justify-start gap-6 text-white font-semibold px-4 text-2xl border-l border-white"
+            aria-label="Slide 3"
+          >
+            <img
+              src="/img/icons/Untitled-1-04-03.svg"
+              className="h-[50px] w-auto mr-4"
+            />
+            <span className="hover:text-slate-100 transition-all">
+              {" "}
+              COVID Testing{" "}
+            </span>
+          </button>
+        </div>
+        <div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+          <div
+            class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none bg-[url(../public/img/woman-collecting-blood-samples-for-testing-on-diff-2022-12-16-06-56-53-utc-scaled-1.jpg)] bg-cover bg-center bg-no-repeat lg:h-[667px]"
+            data-te-carousel-item
+            data-te-carousel-active
+          >
+            <div class="w-full h-full pb-24 mobile-testing-slide">
+              <div className="grid lg:grid-cols-2 py-5 max-w-screen-xl mx-auto pt-12">
+                <div className="flex flex-col gap-6">
+                  <h2 className="text-6xl text-white heading">
+                    Access convenient medical screening at our{" "}
+                    <span className="font-semibold">mobile lab</span>
+                  </h2>
+                  <h4 className="text-white text-lg">
+                    Lab testing with LabCare’s mobile units located throughout
+                    New York City is easy and convenient.
+                  </h4>
+                  <div className="grid md:grid-cols-4 grid-cols-2 w-full">
+                    <div className="flex flex-col items-center gap-2">
+                      <img
+                        className=" w-[72px] h-[72px]"
+                        src="/img/icons/Untitled-1-04-04-1.svg"
+                        alt="General Blood Work"
+                      />
+                      <h5 className="text-white/80 text-sm text-center">
+                        General Blood Work
+                      </h5>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                      <img
+                        className=" w-[72px] h-[72px]"
+                        src="/img/icons/Untitled-1-04-01-2.svg"
+                        alt="COVID/Flu/RSV Testing"
+                      />
+                      <h5 className="text-white/80 text-sm text-center">
+                        COVID/Flu/RSV Testing
+                      </h5>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                      <img
+                        className=" w-[72px] h-[72px]"
+                        src="/img/icons/Untitled-1-04-02-1.svg"
+                        alt="STI Testing"
+                      />
+                      <h5 className="text-white/80 text-sm text-center">
+                        STI Testing
+                      </h5>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                      <img
+                        className=" w-[72px] h-[72px]"
+                        src="/img/icons/Untitled-1-04-05-1.svg"
+                        alt="Medical Screening For Work"
+                      />
+                      <h5 className="text-white/80 text-sm text-center">
+                        Medical Screening For Work
+                      </h5>
+                    </div>
+                  </div>
+                  <div className="flex justify-evenly gap-6">
+                    <a
+                      href="/covid"
+                      className="text-sm px-7 py-3.5 font-semibold rounded flex items-center justify-center border text-white border-white bg-transparent hover:bg-[#0568F6] hover:border-[#0568F6]"
+                    >
+                      LEARN MORE
+                    </a>
+                    <a
+                      href="/covid-mobile-testing"
+                      className="text-sm px-7 py-3.5 font-semibold rounded flex items-center justify-center border text-white border-[#0568F6] bg-[#0568F6] hover:bg-transparent hover:border-white"
+                    >
+                      <AiOutlineSearch className="mr-2 shrink-0 text-lg" />
+                      FIND A MOBILE PATIENT CENTER NEAR YOU
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="grid md:grid-cols-4 grid-cols-2 gap-4 max-w-[725px] mx-auto my-12">
-            <a
-              href="/covid"
-              className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
-            >
-              <img className="w-[100px]" src="/img/icons/Asset-4mdpi.png" />
-              <h3 className="text-blue-secondary text-2xl text-center ">
-                PCR Test
+          <div
+            class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none bg-[url(../public/img/Untitled-2-3.jpg)] bg-cover bg-center bg-no-repeat pb-24 lg:h-[667px]"
+            data-te-carousel-item
+          >
+            <div className="flex flex-col justify-center h-full py-5 max-w-screen-lg mx-auto">
+              <h3 className="text-blue-secondary font-semibold text-xl mb-4">
+                Quick Reliable Results Save Lives
               </h3>
-            </a>
-            <a
-              href="/covid"
-              className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
-            >
-              <img className="w-[100px]" src="/img/icons/Asset-5mdpi.png" />
-              <h3 className="text-blue-secondary text-2xl text-center ">
-                Antigen
-              </h3>
-            </a>
-            <a
-              href="/covid"
-              className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
-            >
-              <img className="w-[100px]" src="/img/icons/Asset-6mdpi.png" />
-              <h3 className="text-blue-secondary text-2xl text-center ">
-                Antibody
-              </h3>
-            </a>
-            <a
-              href="/covid-mobile-testing"
-              className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
-            >
-              <img className="w-[100px]" src="/img/icons/flu-rsv.png" />
-              <h3 className="text-blue-secondary text-2xl text-center ">
-                Winter Panel
-              </h3>
-              <p className="text-blue-secondary text-center">PCR + FLU + RSV</p>
-            </a>
+              <h2 className="text-blue-primary font-bold text-6xl mb-4 heading">
+                Automation
+                <br />
+                and Innovation
+              </h2>
+              <h4 className="text-blue-secondary text-lg mb-4">
+                Fastest Turnaround Time in the Region
+              </h4>
+              <a
+                href="/about"
+                className="mt-8 w-fit rounded-md py-2.5 px-5 font-bold border bg-white text-blue-secondary border-blue-secondary hover:text-white hover:bg-blue-secondary flex items-center"
+              >
+                Learn More <RxCaretRight className="shrink-0 ml-0.5 text-xl" />
+              </a>
+            </div>
           </div>
-          <div className="flex sm:flex-row flex-col items-center justify-center gap-8">
-            <p className="text-blue-primary text-xl">
-              Find a Testing Location Near You
-            </p>
-            <a
-              href="/covid-mobile-testing"
-              className="btn-primary border-blue-primary text-blue-primary border flex items-center justify-center"
-            >
-              <AiOutlineSearch className="mr-2" />
-              Search
-            </a>
+          <div
+            class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none bg-[url(../public/img/landing22.jpg)] bg-cover bg-center bg-no-repeat pb-24 lg:h-[667px]"
+            data-te-carousel-item
+          >
+            <div className="flex flex-col items-center justify-center py-5 max-w-screen-lg mx-auto">
+              <h1 className="heading text-blue-primary font-semibold text-5xl text-center my-4">
+                COVID-19 Mobile Testing Services
+              </h1>
+              <div className="bg-amber-300 p-3">
+                <p className="text-lg font-medium text-center">
+                  ** Please note: Beginning May 10th, a prescription from your
+                  Doctor will be required for covid testing.
+                </p>
+              </div>
+              <div className="grid md:grid-cols-4 grid-cols-2 gap-4 max-w-[725px] mx-auto my-12">
+                <a
+                  href="/covid"
+                  className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
+                >
+                  <img className="w-[100px]" src="/img/icons/Asset-4mdpi.png" />
+                  <h3 className="text-blue-secondary text-2xl text-center ">
+                    PCR Test
+                  </h3>
+                </a>
+                <a
+                  href="/covid"
+                  className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
+                >
+                  <img className="w-[100px]" src="/img/icons/Asset-5mdpi.png" />
+                  <h3 className="text-blue-secondary text-2xl text-center ">
+                    Antigen
+                  </h3>
+                </a>
+                <a
+                  href="/covid"
+                  className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
+                >
+                  <img className="w-[100px]" src="/img/icons/Asset-6mdpi.png" />
+                  <h3 className="text-blue-secondary text-2xl text-center ">
+                    Antibody
+                  </h3>
+                </a>
+                <a
+                  href="/covid-mobile-testing"
+                  className="rounded-lg bg-white flex flex-col items-center justify-center w-full px-1 py-2"
+                >
+                  <img className="w-[100px]" src="/img/icons/flu-rsv.png" />
+                  <h3 className="text-blue-secondary text-2xl text-center ">
+                    Winter Panel
+                  </h3>
+                  <p className="text-blue-secondary text-center">
+                    PCR + FLU + RSV
+                  </p>
+                </a>
+              </div>
+              <div className="flex sm:flex-row flex-col items-center justify-center gap-8">
+                <p className="text-blue-primary text-xl">
+                  Find a Testing Location Near You
+                </p>
+                <a
+                  href="/covid-mobile-testing"
+                  className="btn-primary border-blue-primary text-blue-primary border flex items-center justify-center"
+                >
+                  <AiOutlineSearch className="mr-2 shrink-0" />
+                  Search
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="bg-black/20 max-w-screen-xl rounded-lg mx-auto mt-12 py-4 px-12 md:opacity-100 opacity-0 md:w-full w-0 overflow-hidden sm:block hidden">
-          <div className="grid grid-cols-3 gap-8">
-            <div className="flex items-center justify-end border-r border-white">
-              <a
-                href="#"
-                className="flex items-center w-fit text-white font-semibold px-4 text-2xl"
-              >
-                <img
-                  src="/img/icons/untitled-1-022-01.svg"
-                  className="h-[50px] w-auto mr-4"
-                />
-                <span className="hover:text-slate-100 transition-all">
-                  COVID Testing
-                </span>
-              </a>
-            </div>
-            <div className="flex items-center justify-center">
-              <a
-                href="#"
-                className="flex items-center w-fit text-white font-semibold px-4 text-2xl"
-              >
-                <img
-                  src="/img/icons/labq-icon.png"
-                  className="h-[50px] w-auto mr-4"
-                />
-                <span className="hover:text-slate-100 transition-all">
-                  Automation & Innovation
-                </span>
-              </a>
-            </div>
-            <div className="flex items-center justify-start border-l border-white">
-              <a
-                href="#"
-                className="flex items-center w-fit text-white font-semibold px-4 text-2xl"
-              >
-                <img
-                  src="/img/icons/flu-rsv-icon.png"
-                  className="h-[50px] w-auto mr-4"
-                />
-                <span className="hover:text-slate-100 transition-all">
-                  FLU & RSV
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+      </div>
+
       <section className="py-16 container mx-auto">
         <h2 className="heading text-4xl font-semibold text-center mb-16">
           Lab Specialties
@@ -259,6 +400,9 @@ const Home = () => {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center lg:pr-56 lg:order-2 order-1">
+          <div className="your-lab-animation after:bg-black mb-2 bg-blue-50 px-1 py-0.5">
+            <h3 className="font-bold text-[26px]">{yourLabArr[isVisible]}</h3>
+          </div>
           <h2 className="text-4xl text-center font-medium heading lg:mb-0 mb-16">
             Your Local Lab
           </h2>
